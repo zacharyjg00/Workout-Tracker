8971da9a22616ee666459df93b80951a114d7300
Zacharyjg <guldb016@umn.edu>
2021-08-05 18:31:18 -0500

    Added in totalDuration field when sending info to the clientside

--- server.js
+++ server.js
@@ -24,20 +24,26 @@ app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
+// const workouts = await 
+
 // API Routes
 app.get("/api/workouts", async (req, res) => {
-    const workouts = await db.Workout.find({});
+    const workouts = await db.Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" } } }]);
     res.json(workouts);
 });
 
 app.post("/api/workouts", async (req, res) => {
     db.Workout.create(req.body)
-    .then(newWorkout => res.json(newWorkout));
+        .then(newWorkout => res.json(newWorkout));
 });
 
 app.put("/api/workouts/:id", async (req, res) => {
     db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
-    .then(updatedWorkout => res.json(updatedWorkout));
+        .then(updatedWorkout => res.json(updatedWorkout));
+});
+
+app.get("/api/workouts/range", (req, res) => {
+
 });
 
 app.listen(PORT, () => {
