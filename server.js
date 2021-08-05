61b6c58799956786c02f9c0b9fe3688992773906
Zacharyjg <guldb016@umn.edu>
2021-08-05 18:46:34 -0500

    Added /api/workouts/range get route functionality

--- server.js
+++ server.js
@@ -24,11 +24,14 @@ app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
-// const workouts = await 
-
 // API Routes
 app.get("/api/workouts", async (req, res) => {
     const workouts = await db.Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" } } }]);
+    res.json(workouts);
+});
+
+app.get("/api/workouts/range", async (req, res) => {
+    const workouts = await db.Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" } } }]).limit(7);
     res.json(workouts);
 });
 
@@ -40,10 +43,6 @@ app.post("/api/workouts", async (req, res) => {
 app.put("/api/workouts/:id", async (req, res) => {
     db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
         .then(updatedWorkout => res.json(updatedWorkout));
-});
-
-app.get("/api/workouts/range", (req, res) => {
-
 });
 
 app.listen(PORT, () => {
